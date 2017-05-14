74c32eccefd1f083d1ba4f9a0ec05433a32f4cfa
Nick Lamberth <nslamberth@gmail.com>
2017-05-14 01:06:43 -0400

    update setup.py urls

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     description='Helper functions for generating HTML documents.',
     author='Nick Lamberth',
     author_email='nslamberth@gmail.com',
-    url='https://github.com/nslamberth/htmlfun/tarball/0.1.0',
-    download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+    url='https://github.com/nslamberth/htmlfun/',
+    download_url='https://github.com/nslamberth/htmlfun/archive/master.zip',
     keywords=['html'],  # arbitrary keywords
     classifiers=[],
 )
