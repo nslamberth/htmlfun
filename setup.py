3384aedb67f4767637abb0deb200b73bd7e9d77c
Nick Lamberth <nslamberth@gmail.com>
2017-05-14 00:57:51 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='htmlfun',
+    packages=['htmlfun'],
+    version='0.1.0',
+    description='Helper functions for generating HTML documents.',
+    author='Nick Lamberth',
+    author_email='nslamberth@gmail.com',
+    url='https://github.com/nslamberth/htmlfun/tarball/0.1.0',
+    download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+    keywords=['html'],  # arbitrary keywords
+    classifiers=[],
+)
